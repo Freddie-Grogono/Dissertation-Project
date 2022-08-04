a5faa4063e2f0d03c4374d8ff3bd4585a5ac85f4
Freddie Grogono <91896613+Freddie-Grogono@users.noreply.github.com>
2022-08-04 12:41:59 +0100

    Added de-interleave shell scripts.

---
+++ Bioinformatic-Pipeline/De-Interleave/its2_deinterleave.sh
@@ -0,0 +1,8 @@
+in=grinder_output_files_its2
+
+for f in $(ls $in/sample*.fastq)
+do
+  sample=$(basename $f .fastq)
+  paste - - - - - - - - < $f | tee >(cut -f 1-4 | tr '\t' '\n' > $in/deinterleaved/$sample.R1.fastq) \
+  | cut -f 5-8 | tr '\t' '\n' > $in/deinterleaved/$sample.R2.fastq
+done
